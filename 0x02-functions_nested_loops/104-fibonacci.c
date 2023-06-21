fe9c37d80702c604a48a53e1da35e47d7940d8ba
JohnOlshea <johnolshea@gmail.com>
2023-06-20 18:03:14 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the 1st 50 Fibonacci no. starting with 1
+ *		and 2 followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
