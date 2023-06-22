4e6e5d4e967948d72344bcc2deb446a8a36b9d67
JohnOlshea <johnolshea@gmail.com>
2023-06-22 15:22:05 -0700

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0.
  */
-int print_numbers(void)
+void print_numbers(void)
 {
 	int c;
 
-	for (c = 0; c < 10; c++)
+	for (c = 48; c < 58; c++)
 	{
 		_putchar(c);
 	}
+
 	_putchar('\n');
 }
