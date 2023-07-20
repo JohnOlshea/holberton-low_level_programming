2484c56a78525421da45f9cab4802439d3acc0fe
JohnOlshea <johnolshea@gmail.com>
2023-07-19 19:57:28 -0700

    If you spend too much time thinking

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * int_index
+ * @name: name
+ * @f: function
+ *
+ * Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
